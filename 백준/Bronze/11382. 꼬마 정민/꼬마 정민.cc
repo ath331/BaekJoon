3edc14ee91ad253193d7a25c55ca00eb91018696
ath331 <39950003+ath331@users.noreply.github.com>
2023-06-08 00:46:31 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,22 @@
+/////////////////////////////////////////////////////////////////////
+// @brief    main
+/////////////////////////////////////////////////////////////////////
+
+
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+	long long a = 0;
+	long long b = 0;
+	long long c = 0;
+
+	cin >> a >> b >> c;
+	cout << a + b + c;
+
+	return 0;
+}
