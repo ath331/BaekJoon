31401073c154f46882131c423dfb4638de998ba9
ath331 <39950003+ath331@users.noreply.github.com>
2023-06-08 00:18:36 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,19 @@
+// https://www.acmicpc.net/problem/10926
+// ??!
+
+
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+	string str;
+
+	cin >> str;
+	cout << str + "??!";
+
+	return 0;
+}
