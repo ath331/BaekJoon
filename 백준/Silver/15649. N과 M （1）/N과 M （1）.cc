e4618ae1d3e1ef5aee8608819cdb4aa801658aa2
ath331 <39950003+ath331@users.noreply.github.com>
2023-06-09 21:28:50 +0900

    [Silver III] Title: N과 M (1), Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,45 @@
+/////////////////////////////////////////////////////////////////////
+// @brief    main
+/////////////////////////////////////////////////////////////////////
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+
+int n, m;
+int arr[ 9 ] = { 0, };
+bool visited[ 9 ] = { 0, };
+
+
+void dfs( int cnt )
+{
+	if ( cnt == m )
+	{
+		for ( int i = 0; i < m; i++ )
+			cout << arr[ i ] << ' ';
+		cout << '\n';
+		return;
+	}
+	for ( int i = 1; i <= n; i++ )
+	{
+		if ( !visited[ i ] )
+		{
+			visited[ i ] = true;
+			arr[ cnt ] = i;
+			dfs( cnt + 1 );
+			visited[ i ] = false;
+		}
+	}
+}
+
+int main()
+{
+	cin >> n >> m;
+
+	dfs( 0 );
+}
