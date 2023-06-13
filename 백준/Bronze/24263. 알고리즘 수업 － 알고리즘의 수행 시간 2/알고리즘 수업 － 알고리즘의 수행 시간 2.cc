890c940f3e568cddde2dfa189fe0940b95b18a04
ath331 <39950003+ath331@users.noreply.github.com>
2023-06-13 23:56:15 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,26 @@
+/////////////////////////////////////////////////////////////////////
+// @brief    main
+/////////////////////////////////////////////////////////////////////
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <numeric>
+#include <iomanip>
+
+
+using namespace std;
+
+
+int main()
+{
+	int n = 0;
+	cin >> n;
+
+	cout << n << endl << 1 << endl;
+
+	return 0;
+}
