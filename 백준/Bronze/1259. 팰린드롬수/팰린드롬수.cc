dc9a613c5b89e543db892b5e14b60d9c4c941408
ath331 <39950003+ath331@users.noreply.github.com>
2023-06-24 00:34:22 +0900

    [Bronze I] Title: 팰린드롬수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.cc
@@ -0,0 +1,58 @@
+/////////////////////////////////////////////////////////////////////
+// @brief    main
+/////////////////////////////////////////////////////////////////////
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <numeric>
+#include <iomanip>
+#include <queue>
+#include <stack>
+
+// IO
+#define SYNC_WITH_STDIO_FALSE ios_base::sync_with_stdio( false )
+#define CIN_TIE_NULLPTR cin.tie( nullptr )
+#define IO_SPEED_UPGRADE SYNC_WITH_STDIO_FALSE; CIN_TIE_NULLPTR;
+
+// Loop
+#define FOR_INDEX( size ) for( int i = 0 ; i < size ; i++ )
+#define FOR_INDEX_SECOND( size ) for( int j = 0 ; j < size ; j++ )
+#define FOR_RANGE( container ) for( auto item : container )
+#define FOR_RANGE_SECOND( container ) for( auto item2 : container )
+#define FOR_RANGE_REF( container ) for( auto& itemRef : container )
+#define FOR_RANGE_SECOND_REF( container ) for( auto& itemRef2 : container )
+
+// Code
+#define MAIN_BEGIN int main() { IO_SPEED_UPGRADE; 
+#define MAIN_END return 0; }
+
+
+using namespace std;
+
+
+MAIN_BEGIN
+{
+	int num = 0;
+
+	while ( true )
+	{
+		cin >> num;
+		if ( !num )
+			break;
+
+		string str = to_string( num );
+		string temp = str;
+		reverse( str.begin(), str.end() );
+
+		if ( str == temp )
+			cout << "yes\n";
+		else
+			cout << "no\n";
+	}
+}
+MAIN_END
