a39a1f200cd987ae4fb7ec83d173aabe0c5cfa81
ath331 <39950003+ath331@users.noreply.github.com>
2023-06-26 23:14:43 +0900

    [Silver IV] Title: 등수 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.cc
@@ -0,0 +1,73 @@
+/////////////////////////////////////////////////////////////////////
+// @brief    main
+/////////////////////////////////////////////////////////////////////
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <numeric>
+#include <iomanip>
+#include <queue>
+#include <deque>
+#include <stack>
+
+// IO
+#define SYNC_WITH_STDIO_FALSE ios_base::sync_with_stdio( false )
+#define CIN_TIE_NULLPTR cin.tie( nullptr )
+#define IO_SPEED_UPGRADE SYNC_WITH_STDIO_FALSE; CIN_TIE_NULLPTR;
+
+// Loop
+#define FOR_INDEX( size ) for( int i = 0 ; i < size ; i++ )
+#define FOR_INDEX_SECOND( size ) for( int j = 0 ; j < size ; j++ )
+#define FOR_RANGE( container ) for( auto item : container )
+#define FOR_RANGE_SECOND( container ) for( auto item2 : container )
+#define FOR_RANGE_REF( container ) for( auto& itemRef : container )
+#define FOR_RANGE_SECOND_REF( container ) for( auto& itemRef2 : container )
+
+// Code
+#define MAIN_BEGIN int main() { IO_SPEED_UPGRADE;
+#define MAIN_END return 0; }
+
+
+using namespace std;
+
+
+MAIN_BEGIN
+{
+	int N, score, P;
+	int rank[ 100 ];
+
+	cin >> N >> score >> P;
+
+	for ( int i = 0; i < N; i++ )
+	{
+		cin >> rank[ i ];
+	}
+
+	int cnt = 0;
+	int my_rank = 1;
+	for ( int i = 0; i < N; i++ )
+	{
+		if ( score < rank[ i ] ) 
+			my_rank += 1;
+		else if ( score == rank[ i ] ) 
+			my_rank = my_rank;
+		else 
+			break;
+
+		cnt++;
+	}
+
+
+	if ( cnt == P ) 
+		my_rank = -1;
+	if ( N == 0 ) 
+		my_rank = 1;
+
+	cout << my_rank;
+}
+MAIN_END
