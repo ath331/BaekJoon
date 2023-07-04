dbbe8de60db100a306b71078aff1774fe55e9738
ath331 <39950003+ath331@users.noreply.github.com>
2023-07-04 20:28:45 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
@@ -0,0 +1,63 @@
+/////////////////////////////////////////////////////////////////////
+// @brief    main
+/////////////////////////////////////////////////////////////////////
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <numeric>
+#include <iomanip>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <regex>
+
+// IO
+#define SYNC_WITH_STDIO_FALSE ios_base::sync_with_stdio( false )
+#define CIN_TIE_NULLPTR cin.tie( NULL )
+#define IO_SPEED_UPGRADE SYNC_WITH_STDIO_FALSE; CIN_TIE_NULLPTR;
+
+// Loop
+#define FOR_INDEX( size ) for( int forI = 0 ; forI < size ; forI++ )
+#define FOR_INDEX_SECOND( size ) for( int forJ = 0 ; forJ < size ; forJ++ )
+#define FOR_RANGE( container ) for( auto item : container )
+#define FOR_RANGE_SECOND( container ) for( auto item2 : container )
+#define FOR_RANGE_REF( container ) for( auto& itemRef : container )
+#define FOR_RANGE_SECOND_REF( container ) for( auto& itemRef2 : container )
+
+// Code
+#define INT_( x ) int x = 0; cin >> x;
+#define STR_( str ) string str; cin >> str;
+#define MAIN_BEGIN int main() { IO_SPEED_UPGRADE;
+#define MAIN_END return 0; }
+
+
+using namespace std;
+
+
+MAIN_BEGIN
+{
+	INT_( N );
+	INT_( M );
+
+	vector< int > v;
+
+	FOR_INDEX( N )
+		v.push_back( forI + 1 );
+	
+	FOR_INDEX( M )
+	{
+		INT_( i );
+		INT_( j );
+
+		std::iter_swap( v.begin() + ( i - 1 ) , v.begin() + ( j - 1 ) );
+	}
+
+	FOR_RANGE( v )
+		cout << item << " ";
+}
+MAIN_END
