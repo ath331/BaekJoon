0a5c92501939e58b46e9764d0776f3c049a8c899
ath331 <39950003+ath331@users.noreply.github.com>
2023-07-09 18:31:51 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,50 @@
+/////////////////////////////////////////////////////////////////////
+// @brief    main
+/////////////////////////////////////////////////////////////////////
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <numeric>
+#include <iomanip>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <regex>
+
+// IO
+#define SYNC_WITH_STDIO_FALSE ios_base::sync_with_stdio( false )
+#define CIN_TIE_NULLPTR cin.tie( NULL )
+#define IO_SPEED_UPGRADE SYNC_WITH_STDIO_FALSE; CIN_TIE_NULLPTR;
+
+// Loop
+#define FOR_INDEX( size ) for( int forI = 0 ; forI < size ; forI++ )
+#define FOR_INDEX_SECOND( size ) for( int forJ = 0 ; forJ < size ; forJ++ )
+#define FOR_RANGE( container ) for( auto item : container )
+#define FOR_RANGE_SECOND( container ) for( auto item2 : container )
+#define FOR_RANGE_REF( container ) for( auto& itemRef : container )
+#define FOR_RANGE_SECOND_REF( container ) for( auto& itemRef2 : container )
+#define FOR_ITER( container ) for( auto& iter :  container)
+#define FOR_ITER_SECOND( container ) for( auto& iter2 :  container)
+
+// Code
+#define INT_( x ) int x = 0; cin >> x;
+#define STR_( str ) string str; cin >> str;
+#define MAIN_BEGIN int main() { IO_SPEED_UPGRADE;
+#define MAIN_END return 0; }
+
+
+using namespace std;
+
+
+MAIN_BEGIN
+{
+	STR_( str );
+
+	cout << str.size();
+}
+MAIN_END
