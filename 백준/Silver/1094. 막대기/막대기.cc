d394b272babbf4af9a36fb43638364f7b0053e98
ath331 <39950003+ath331@users.noreply.github.com>
2023-08-09 21:10:25 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -0,0 +1,20 @@
+//백준1094 막대기
+ 
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int X;
+    cin >> X;
+ 
+    int ans = 0;
+    while (X>0) {
+        if (X % 2 == 1) {
+            ans++;
+        }
+        X /= 2;
+    }
+ 
+    cout << ans;
+ 
+}
