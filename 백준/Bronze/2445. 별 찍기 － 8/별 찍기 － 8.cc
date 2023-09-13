c0b6b9d4d55cca954f515b25786c7e120a097278
ath331 <39950003+ath331@users.noreply.github.com>
2023-09-13 22:53:52 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <=i; j++)
+		{
+			cout << "*";
+		}
+		for (int j = 1; j <=2*(n-i); j++)
+		{
+			cout << " ";
+		}
+		for (int j = 1; j <= i; j++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+	for (int i = 1; i<=n-1; i++)
+	{
+		for (int j = i; j <n; j++)
+		{
+			cout << "*";
+		}
+		for (int j = 1; j <= 2*i; j++)
+		{
+			cout << " ";
+		}
+		for (int j = i; j <n; j++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+
+}
