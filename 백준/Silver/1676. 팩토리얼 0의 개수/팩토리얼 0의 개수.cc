7dac4017f910990e5bfea9757b9fe5cea553ce28
ath331 <39950003+ath331@users.noreply.github.com>
2023-10-24 21:08:26 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cc
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n;
+	int ans = 0;
+	cin >> n;
+
+	for (int i = 5; i <= n; i *= 5) {
+		ans += n / i;
+	}
+	cout << ans;
+
+	return 0;
+}
