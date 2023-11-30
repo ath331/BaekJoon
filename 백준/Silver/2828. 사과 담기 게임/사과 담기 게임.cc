80d5d57bb80aa3d894888eb1f71fcaf046c30181
ath331 <39950003+ath331@users.noreply.github.com>
2023-11-30 19:14:17 +0900

    [Silver V] Title: 사과 담기 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int N, M, J;
+
+int main() {
+    cin >> N >> M >> J;
+
+    int cnt=0;
+
+    int start=1, end = M;
+
+    while(J--) {
+        int x;
+        cin >> x;
+
+        bool flag=true;
+        while(flag) {
+            if(start<=x && x<=end) {
+                flag=false;
+            } else if(start>x){
+                start--; end--; cnt++;
+            } else {
+                start++; end++; cnt++;
+            }
+        }
+    }
+
+    cout << cnt;
+}
