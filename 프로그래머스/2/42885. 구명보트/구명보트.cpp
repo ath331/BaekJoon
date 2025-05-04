3218a593a0a9433e4813be252b0d1c085b550961
ath331 <39950003+ath331@users.noreply.github.com>
2025-05-04 17:26:09 +0900

    [level 2] Title: 구명보트, Time: 1.99 ms, Memory: 4.73 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+	using namespace std;
+
+	int solution( vector<int> people, int limit )
+	{
+		int answer = 0;
+
+		sort( people.begin(), people.end() );
+
+		int left = 0;
+		int right = people.size() - 1;
+
+		while ( left <= right )
+		{
+			if ( people[ left ] + people[ right ] <= limit )
+			{
+				left++;
+			}
+
+			right--;
+			answer++;
+		}
+
+		return answer;
+	}
